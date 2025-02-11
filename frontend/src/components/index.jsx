004e5a19f5e6c7b9d22d5ec81084b04958a22346
aitievan <124905950+aitievan@users.noreply.github.com>
2025-02-11 22:38:54 +0600

    Добавил изменения

---
+++ frontend/src/components/index.jsx
@@ -0,0 +1,9 @@
+import {Button} from "./Button";
+import {CheckBox} from "./CheckBox";
+import {Heading} from "./Heading";
+import {Img} from "./Img";
+import {Input} from "./Input";
+import {Slider} from "./Slider";
+import {Text} from "./Text";
+
+export{Button,CheckBox,Heading,Img,Input,Slider,Text };
